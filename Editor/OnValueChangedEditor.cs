3622d73ecd434ef6c80d4d28d99bd2e24503afbc
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-07-04 09:50:10 +0200

    ⠀⠀⠀⠀⠀

--- Editor/OnValueChangedEditor.cs
+++ Editor/OnValueChangedEditor.cs
@@ -69,7 +69,6 @@ namespace UnityEssentials
 
                 foreach (var property in allProperties)
                 {
-                    Debug.Log($"Checking property: {property.name} for method: {method.Name}");
                     if (!attribute.FieldNames.Any(fieldName => fieldName == property.name))
                         continue;
 
